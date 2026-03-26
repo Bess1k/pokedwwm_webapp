8e4c4421151124ceae3b44e94f2eba6095b9f3e7
Besnik MEHMETI <besnik.mehmeti@ccicampus.fr>
2026-03-26 10:07:27 +0100

    Creation d'un filter Twig

--- src/Twig/Components/BootstrapButton.php
+++ src/Twig/Components/BootstrapButton.php
@@ -8,17 +8,26 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 final class BootstrapButton
 {
     private string $_strText;
-    private string $_strType;
+    private string $_strType= "";// Initialise à une chaine vide dans le cas oû outlined -false
     private string $_strLink;
 
     /**
      * Mont le composant d ebutton Booststrap
+     * @param string $text Texte affiché dans le bouton
+     * @param string $type Type de bouton: succes, primary
+     * @param string $link
+     * @param bool ùoutlined
+     * 
      */
-    public function mount(string $text, string $type, string $link): void
+    public function mount(string $text, string $type, string $link, bool $outlined = false): void
     {
         $this->_strText = $text;
-        $this->_strType = $type;
         $this->_strLink = $link;
+
+        if($outlined){
+            $this->_strType = 'outlibe-';
+        }
+        $this->_strType = $type;
     }
 
     /**
