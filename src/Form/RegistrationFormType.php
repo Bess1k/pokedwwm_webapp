bc239f9b923bc5ebb31adf8a05c0dcdd3e5f33ba
Besnik MEHMETI <besnik.mehmeti@ccicampus.fr>
2026-04-07 13:08:08 +0200

    RepeatedType pour le mot de passe (inscription)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,46 +15,57 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('birthdate')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail de connexion'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('birthdate', null, [
+                'label' => 'Date de naissance'
+            ])
 
             ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
+                'mapped'        => false,
+                'label'         => "Accepter les conditions d'utilisation",
+                'constraints'   => [
                     new IsTrue(
-                        message: 'You should agree to our terms.',
+                        message: 'Vous devez accepter les conditions',
                     ),
                 ],
             ])
-            
+
             ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'mapped' => false,
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped'            => false,
+                'type'              => PasswordType::class,
+
+                'invalid_message'   => 'Les champs doivent être identiques', 
+                'required'          => true,
+
+                'first_options'     => ['label' => 'Mot de passe'],
+                'second_options'    => ['label' => 'Confirmer le mot de passe'],
+
                 'attr' => ['autocomplete' => 'new-password'],
-                'invalid_message' => 'Les mots de passe doivent être identiques.',
-                'first_options'  => [
-                    'label' => 'Mot de passe',
-                    'constraints' => [
-                        new NotBlank(
-                            message: 'Please enter a password',
-                        ),
-                        new Length(
-                            min: 6,
-                            minMessage: 'Your password should be at least {{ limit }} characters',
-                            max: 4096,
-                        ),
-                    ],
-                ],
-                'second_options' => [
-                    'label' => 'Confirmation du mot de passe',
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Saisir un mot de passe',
+                    ),
+                    new Length(
+                        min: 6,
+                        minMessage: 'Votre mot de passe doit faire au minimum {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        max: 4096,
+                    ),
                 ],
             ])
         ;
