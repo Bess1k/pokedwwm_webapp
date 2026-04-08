11f56f9976e9f560ee490f3036d7e9e0532883cf
Bess1k <bmehmeti@gmail.com>
2026-04-08 09:43:18 +0200

    User Création et modif (admin)

--- src/Controller/UsercontrollerController.php
+++ src/Controller/UsercontrollerController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UsercontrollerController extends AbstractController
 {
-    #[Route('/usercontroller', name: 'app_usercontroller')]
-    public function index(): Response
+    #[Route('/users', name: 'app_users')]
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
         return $this->render('usercontroller/index.html.twig', [
-            'controller_name' => 'UsercontrollerController',
+            'users' => $users,
         ]);
     }
 }
